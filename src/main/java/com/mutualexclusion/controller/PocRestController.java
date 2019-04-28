1f10deaafc69461098c6da2490f95dec89596b0c
tandonraghav <tandonraghavs@gmail.com>
2019-04-29 01:01:18 +0530

    Adding jar file

--- src/main/java/com/mutualexclusion/controller/PocRestController.java
+++ src/main/java/com/mutualexclusion/controller/PocRestController.java
@@ -24,7 +24,7 @@ public class PocRestController {
         riccartAgarwalaME.request(createMessage(),systemSettings.getNodeId());
         riccartAgarwalaME.executeCS(null);
 
-        return "Working";
+        return "";
     }
 
     private Message createMessage(){
